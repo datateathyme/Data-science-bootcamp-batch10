9e63e4064a6387f25440f003bdd0769c0d134300
sunsunbola <chanakarn.data789@gmail.com>
2025-06-24 14:46:32 +0700

    Create data_frame.R

---
+++ R-programming/data_frame.R
@@ -0,0 +1,18 @@
+## Data Frame
+
+friends <- c("Bae","Jay","May","Bec","Fay")
+
+ages <- c(22,23,25,22,24)
+
+location <- c("New York", "London", "London",
+              "Tokyo", "Manchester")
+
+movie_lover <- c(TRUE, TRUE, FALSE, 
+                 TRUE ,TRUE)
+
+df <- data.frame(friends,
+          ages,
+          location,
+          movie_lover)
+
+View(df)
